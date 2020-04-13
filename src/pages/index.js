4d578a1ff861b5333a82ad74fab48fa42f7f4eb3
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-04-13 23:41:03 +0000

    linter update & watch button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,25 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <h1>Learn to design and code React apps</h1>
+    <p>
+      Complete course about the best tools and design systems.
+      Prototype and build apps with React and Swift
+    </p>
+
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
 
-export default IndexPage
+    <Link to="/page-2/">Watch the video</Link>
+  </Layout>
+);
+
+export default IndexPage;
