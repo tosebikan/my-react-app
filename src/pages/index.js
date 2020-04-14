33374642196a55670b9df20c0f7562de51d24920
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-04-14 11:59:26 +0000

    header complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
+// import Image from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Learn to design and code React apps</h1>
-    <p>
-      Complete course about the best tools and design systems.
-      Prototype and build apps with React and Swift
-    </p>
-
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>
+          Complete course about the best tools and design systems.
+          Prototype and build apps with React and Swift
+        </p>
+        <Link to="/page-2/">Watch the video</Link>
+        {/*  <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+          <Image />
+        </div> */}
+      </div>
     </div>
-
-    <Link to="/page-2/">Watch the video</Link>
   </Layout>
 );
 
